bbbbd7de476799d44efb312b87364bd249e7bb20
dalespiteri <dalespiteri@gmail.com>
2018-02-02 16:37:40 -0500

    sub page added to remain behind new page animation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      activePage: 'home'
+      activePage: 'home',
+      subActivePage: ''
     };
     this.showSkills = this.showSkills.bind(this);
     this.showPortfolio = this.showPortfolio.bind(this);
@@ -23,36 +24,49 @@ class App extends Component {
 
   showSkills() {
     this.setState({
+      subActivePage: this.state.activePage,
       activePage: 'skills'
     });
   }
 
   showPortfolio() {
     this.setState({
+      subActivePage: this.state.activePage,
       activePage: 'portfolio'
     });
   }
 
   showAbout() {
     this.setState({
+      subActivePage: this.state.activePage,
       activePage: 'about'
     });
   }
 
   showContact() {
     this.setState({
+      subActivePage: this.state.activePage,
       activePage: 'contact'
     });
   }
 
   render() {
     return (
-      <div>
+      <div className="masterContainer">
+      <div className="subActivePage">
+      { this.state.subActivePage === 'home' ? <Home /> : null }
+      { this.state.subActivePage === 'skills' ? <Skills /> : null }
+      { this.state.subActivePage === 'portfolio' ? <Portfolio /> : null }
+      { this.state.subActivePage === 'about' ? <About /> : null }
+      { this.state.subActivePage === 'contact' ? <Contact /> : null }
+      </div>
+      <div className="activePage">
       { this.state.activePage === 'home' ? <Home /> : null }
       { this.state.activePage === 'skills' ? <Skills /> : null }
       { this.state.activePage === 'portfolio' ? <Portfolio /> : null }
       { this.state.activePage === 'about' ? <About /> : null }
       { this.state.activePage === 'contact' ? <Contact /> : null }
+      </div>
       <Menu
         skills={ this.showSkills }
         portfolio= { this.showPortfolio }
