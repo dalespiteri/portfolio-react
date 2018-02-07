a9be3dd448d242fae26598d16a28d71255e286b1
dalespiteri <dalespiteri@gmail.com>
2018-02-06 23:11:26 -0500

    portfolio general structure and style added

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './style/contact.css';
+import logoBandW from './images/logoBandW.svg';
+import gitHub from './images/gitHub.svg';
+import linkedIn from './images/linkedIn.svg';
+import googlePlus from './images/googlePlus.svg';
 
 class Contact extends Component {
   render() {
 
     return (
       <div className={'contactContainer pageContainer ' + this.props.className} >
+
+        <div className="contactContent content">
+            <img src={logoBandW} className="contactLogo" alt="black and white pixel face logo" />
+            <div className="contactEmail">
+                <p>dalespiteri@gmail.com</p>
+            </div>
+            <ul className="contactLogoList">
+                <a href="http://www.linkedin.com/in/dalespiteri" target="_blank"><li><img src={linkedIn} className="contactSubLogo" alt="linked in logo" /></li></a>
+                <a href="https://github.com/dalespiteri" target="_blank"><li><img src={gitHub} className="contactSubLogo" alt="git hub logo" /></li></a>
+                <a href="https://plus.google.com/+DaleSpiteri" target="_blank"><li><img src={googlePlus} className="contactSubLogo" alt="google plus logo" /></li></a>
+            </ul>
+        </div>
 
       </div>
   );
--- portfolio/src/Portfolio.js
+++ portfolio/src/Portfolio.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import './style/portfolio.css';
+import bobsBurgerThumb from './images/bobsBurgers.jpg';
+import lanPartyThumb from './images/lanParty.jpg';
+import lowThumb from './images/lordsOfWar.jpg';
 
 class Portfolio extends Component {
   render() {
 
     return (
       <div className={"portfolioContainer pageContainer " + this.props.className}>
+
+        <div className="portfolioContent content" >
+          <div className="header">
+            <h1>Portfolio</h1>
+          </div>
+          <div className="webpages">
+            <div className="webpage bobsBurgers">
+              <img src={bobsBurgerThumb} alt="bobs burgers webpage"/>
+              <div className="webpageHover">
+                <div className="webpageInfo">
+                  <h2>Bob's Burgers: Restaurant Site</h2>
+                  <p>A restaurant site based off the fictional Fox television series, Bob’s Burgers. Includes a landing page, locations, menu, and about section created using Bootstrap.</p>
+                  <a href="http://dalespiteri.github.io" target="_blank">
+                    <button className="fullSiteLink">visit full site</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="webpage lanParty">
+              <img src={lanPartyThumb} alt="lan party web page"/>
+              <div className="webpageHover">
+                <div className="webpageInfo">
+                  <h2>The LAN Party: Podcast Site</h2>
+                  <p>A single page website that uses jQuery to navigate an info panel, cast panel, latest episode, and episode list panel.</p>
+                  <a href="lanPartySite/index.html" target="_blank">
+                    <button className="fullSiteLink">visit full site</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="webpage lordsOfWar">
+              <img src={lowThumb} alt="lords of war webpage"/>
+              <div className="webpageHover">
+                <div className="webpageInfo">
+                  <h2>Lords of War: Store Site</h2>
+                  <p>A website for the Hobby store in Oakville, Lords of War: Games and Hobbies. My first completed site.</p>
+                  <a href="lowSite/index.html" target="_blank">
+                    <button className="fullSiteLink">visit full site</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </div>
   );
