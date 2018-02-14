8f83650f54b2f44a7d10cfd91ee05e903588fa55
dalespiteri <dalespiteri@gmail.com>
2018-02-14 11:29:33 -0500

    media queries for portfolio and skills section

--- portfolio/src/HTML.js
+++ portfolio/src/HTML.js
@@ -17,6 +17,9 @@ class HTML extends Component {
             <img src={Skull} alt="proud naked guy" />
           </div>
         </div>
+        <div className="mobileHeader">
+          <h3>the skeleton</h3>
+        </div>
         <div className="skillBottom">
           <p>I actually first learned HTML as a kid. Making sites with friends using Geocities and Angelfire about videogames we used to play, I learned the basic building blocks of a webpage. It was a hobby for a couple years and it's amazing to see how much I had left unturned now that I am relearning it.</p>
         </div>
--- portfolio/src/Javascript.js
+++ portfolio/src/Javascript.js
@@ -17,6 +17,9 @@ class Javascript extends Component {
             <img src={Muscles} alt="strong guy" />
           </div>
         </div>
+        <div className="mobileHeader">
+          <h3>the muscles</h3>
+        </div>
         <div className="skillBottom">
           <p>I enjoy challenges, and Javascript is a problem solver's dream. There's always a way to manipulate your page structure and style in fascinating ways, but at times, it takes a lot of creativity.</p>
           <p>As a kid, I always enjoyed logic puzzles. You remember those right? You'd get a few paragraphs of a story, then a grid table with possibly a list of people, and a list of objects. You'd then have to use the information of the story to pair each person with an object using logical eliminations. Javascript reminds me of those. Sometimes you build from the front, sometimes you build from the back, sometimes you build from the middle. It's like building a Rube Goldberg contraption with language.</p>
