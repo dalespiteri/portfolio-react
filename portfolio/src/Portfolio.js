efeb5122262fe52adc951b7ec38514d90035fee4
dalespiteri <dalespiteri@gmail.com>
2018-02-17 01:14:57 -0500

    final style touches, added score keeper portfolio, adjust package.json to build for dalespiteri.com

--- portfolio/src/Portfolio.js
+++ portfolio/src/Portfolio.js
@@ -4,6 +4,7 @@ import bobsBurgerThumb from './images/bobsBurgers.jpg';
 import lanPartyThumb from './images/lanParty.jpg';
 import lowThumb from './images/lordsOfWar.jpg';
 import triviaThumb from './images/trivia.png';
+import scoreKeeperThumb from './images/scoreKeeper.png';
 
 class Portfolio extends Component {
   render() {
@@ -15,6 +16,9 @@ class Portfolio extends Component {
           <div className="header">
             <h1>Portfolio</h1>
           </div>
+          <a href="https://github.com/dalespiteri/portfolio-react" target="_blank">
+            <button className="portfolioGitRepoButton">view the github repository for this site</button>
+          </a>
           <div className="webpages">
             <div className="webpage triviaPage">
               <div>
@@ -24,10 +28,27 @@ class Portfolio extends Component {
                 <div>
                   <h2>Trivia: Random Trivia Game</h2>
                   <p>A ReactJS application that uses the Open Trivia API to pull random questions in your selected category.</p>
-                  <a href="http://dalespiteri.github.io" target="_blank">
+                  <a href="http://dalespiteri.github.io/reactTrivia/index.html" target="_blank">
                     <button className="fullSiteLink">full site</button>
                   </a>
-                  <a href="" target="_blank">
+                  <a href="https://github.com/dalespiteri/react-trivia" target="_blank">
+                    <button className="gitRepoButt">repository</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="webpage scoreKeeperPage">
+              <div>
+                <img src={scoreKeeperThumb} alt="trivia webpage"/>
+              </div>
+              <div className="webpageInfo">
+                <div>
+                  <h2>Score Keeper</h2>
+                  <p>A score keeping application using Javascript and object oriented programming.</p>
+                  <a href="https://dalespiteri.github.io/scoreKeeper/index.html" target="_blank">
+                    <button className="fullSiteLink">full site</button>
+                  </a>
+                  <a href="https://github.com/dalespiteri/scoreKeeper" target="_blank">
                     <button className="gitRepoButt">repository</button>
                   </a>
                 </div>
@@ -41,10 +62,10 @@ class Portfolio extends Component {
                 <div>
                   <h2>Bob's Burgers: Restaurant Site</h2>
                   <p>A restaurant site based off the fictional Fox television series, Bob’s Burgers. Includes a landing page, locations, menu, and about section created using Bootstrap.</p>
-                  <a href="http://dalespiteri.github.io" target="_blank">
+                  <a href="http://dalespiteri.github.io/bobsBurgers/index.html" target="_blank">
                     <button className="fullSiteLink">full site</button>
                   </a>
-                  <a href="" target="_blank">
+                  <a href="https://github.com/dalespiteri/bobs-burgers" target="_blank">
                     <button className="gitRepoButt">repository</button>
                   </a>
                 </div>
@@ -56,10 +77,10 @@ class Portfolio extends Component {
                 <div className="webpageInfo">
                   <h2>The LAN Party: Podcast Site</h2>
                   <p>A single page website that uses jQuery to navigate an info panel, cast panel, latest episode, and episode list panel.</p>
-                  <a href="lanPartySite/index.html" target="_blank">
+                  <a href="http://dalespiteri.github.io/theLanParty/index.html" target="_blank">
                     <button className="fullSiteLink">full site</button>
                   </a>
-                  <a href="" target="_blank">
+                  <a href="https://github.com/dalespiteri/thelanparty" target="_blank">
                     <button className="gitRepoButt">repository</button>
                   </a>
                 </div>
@@ -71,10 +92,10 @@ class Portfolio extends Component {
                 <div className="webpageInfo">
                   <h2>Lords of War: Store Site</h2>
                   <p>A website for the Hobby store in Oakville, Lords of War: Games and Hobbies. My first completed site.</p>
-                  <a href="lowSite/index.html" target="_blank">
+                  <a href="http://dalespiteri.github.io/lordsOfWar/index.html" target="_blank">
                     <button className="fullSiteLink">full site</button>
                   </a>
-                  <a href="" target="_blank">
+                  <a href="https://github.com/dalespiteri/lordsofwar" target="_blank">
                     <button className="gitRepoButt">repository</button>
                   </a>
                 </div>
