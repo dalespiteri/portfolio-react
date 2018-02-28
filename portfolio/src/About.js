8b8481c97ff5a691018131821c9620daa1bad608
dalespiteri <dalespiteri@gmail.com>
2018-02-28 01:59:59 -0500

    header style change, form submit button animation

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -14,26 +14,28 @@ class About extends Component {
           <div className="header">
             <h1>About Me</h1>
           </div>
-          <div className="aboutText">
-            <div className="outterBubble">
-              <p>Hi, I’m Dale, and I was a designer.</p>
-            </div>
-            <div className="bubbleConnector"></div>
-            <div className="aboutSectionOne">
-              <div className="aboutParagraph">
-                <p>I’ve spent the last ten years as a professional graphic designer. I love design, but working with a static canvas can start to feel a bit dull. I want to add functionality. What could suit a graphic designer looking to add function to form better than web development?</p>
+          <div className="aboutBlurbContainer">
+            <div className="aboutText">
+              <div className="outterBubble">
+                <p>Hi, I’m Dale, and I was a designer.</p>
               </div>
-              <div className="aboutImage aboutImageOne"><img src={aboutHead} alt="charicature head"/></div>
-            </div>
-            <div className="aboutSectionTwo">
-              <div className="aboutImage aboutImageTwo"><img src={aboutHeadTwo} alt="charicature head"/></div>
-              <div className="aboutParagraph">
-                <p>So I researched. I looked at post graduate programs. I looked at boot camps. I looked at self teaching. I asked people in the field. Overwhelmingly, I was pushed to teach myself. Why? Because learning doesn’t stop. If you can’t teach yourself, you’ll eventually be phased out. So I did that. I taught myself to code. I’m still teaching myself to code. I’ll always be teaching myself because new and exciting tools will continue to be developed and at my disposal.</p>
+              <div className="bubbleConnector"></div>
+              <div className="aboutSectionOne">
+                <div className="aboutParagraph">
+                  <p>I’ve spent the last ten years as a professional graphic designer. I love design, but working with a static canvas can start to feel a bit dull. I want to add functionality. What could suit a graphic designer looking to add function to form better than web development?</p>
+                </div>
+                <div className="aboutImage aboutImageOne"><img src={aboutHead} alt="charicature head"/></div>
               </div>
-            </div>
-            <div className="bubbleConnector"></div>
-            <div className="outterBubble">
-              <p>So now, I’m Dale, and I'm a web developer and designer.</p>
+              <div className="aboutSectionTwo">
+                <div className="aboutImage aboutImageTwo"><img src={aboutHeadTwo} alt="charicature head"/></div>
+                <div className="aboutParagraph">
+                  <p>So I researched. I looked at post graduate programs. I looked at boot camps. I looked at self teaching. I asked people in the field. Overwhelmingly, I was pushed to teach myself. Why? Because learning doesn’t stop. If you can’t teach yourself, you’ll eventually be phased out. So I did that. I taught myself to code. I’m still teaching myself to code. I’ll always be teaching myself because new and exciting tools will continue to be developed and at my disposal.</p>
+                </div>
+              </div>
+              <div className="bubbleConnector"></div>
+              <div className="outterBubble">
+                <p>So now, I’m Dale, and I'm a web developer and designer.</p>
+              </div>
             </div>
           </div>
         </div>
